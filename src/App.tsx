b4cdd3b217a0e8a0e9e4d0260bcf8f7dc74e0c08
IvanMinets <minetssmash@gmail.com>
2023-04-10 21:36:50 +0300

    Update componenys, code refactoring, added input settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import React, {useState} from 'react';
 import './App.css';
 import Todolist, {TaskType} from "./Todolist";
+import {v1} from "uuid";
+
+
+
 
 export type FilterValuesType = "all" | "active" | "completed"
 
 function App() {
     const [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
     ])
 
-    const[filter, setFilter] = useState<FilterValuesType>("completed")
+    const[filter, setFilter] = useState<FilterValuesType>("all")
     const changeFilter = (nextFilter: FilterValuesType) => {
         setFilter(nextFilter)
     }
-
-
-
-    const removeTask = (taskId: number) => {
+    const removeTask = (taskId: string) => {
         setTasks(tasks.filter(t => t.id !== taskId))
-
     }
+    const addTask = (title: string) => {
+        const newTask: TaskType = {
+            id: v1(),
+            title: title,
+            isDone: false
+        }
+        setTasks([newTask, ...tasks])
+    }
+
     const getTasksForMe = (tasksList: Array<TaskType>, filterValue: FilterValuesType) => {
         switch (filterValue) {
             case "active":
-                return tasks.filter(t => t.isDone === false)
+                return tasks.filter(t => !t.isDone)
             case "completed":
-                return tasks.filter(t => t.isDone === true)
+                return tasks.filter(t => t.isDone)
             default:
                 return tasks
         }
@@ -41,6 +50,7 @@ function App() {
                       tasks={tasksWhatIWantToSee}
                       removeTask={removeTask}
                       changeFilter = {changeFilter}
+                      addTask={addTask}
             />
         </div>
     );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,43 +1,73 @@
-import React from "react";
+import React, {ChangeEvent, useRef, useState, KeyboardEvent} from "react";
 import {FilterValuesType} from "./App";
 
 type PropsType = {
     title: string
     tasks: TaskType[]
-    removeTask: (taskId: number) => void
+    removeTask: (taskId: string) => void
+    addTask: (title: string) => void
     changeFilter: (nextFilter: FilterValuesType) => void
+
 }
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 
 export const Todolist = (props: PropsType) => {
+    const [title,setTitle] = useState<string>('')
+    console.log(title)
+    // const taskTitleInput = useRef<HTMLInputElement>(null)
+    // const addTaskHandler = () => {
+    //     if (taskTitleInput.current) {
+    //         props.addTask(taskTitleInput.current.value)
+    //         taskTitleInput.current.value = ""
+    //     }
+    // }
 
-
-    const tasksJSXElements: Array<JSX.Element> = props.tasks.map((task:TaskType) => {
-        return (<li><input type ="checkbox" checked={task.isDone}/>
+    const setTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+    const addTaskHandler = ()=>{
+        props.addTask(title)
+        setTitle("")
+    }
+    const tasksJSXElements: Array<JSX.Element> = props.tasks.map((task: TaskType) => {
+        return (<li><input type="checkbox" checked={task.isDone}/>
             <span>{task.title}</span>
-            <button onClick={() =>props.removeTask(task.id)}>x</button>
+            <button onClick={() => props.removeTask(task.id)}>x</button>
         </li>)
     })
-
+    const titleMaxLength: number = 25;
+    const isTitleLengthTooLong: boolean = title.length > titleMaxLength
+    const isAddBtnDisabled: boolean = !title.length || isTitleLengthTooLong
+    const titleMaxLengthWarning = isTitleLengthTooLong ? <div style={{color: "red"}}>Title is too long</div> : null
+    const handlerCreator = (filter: FilterValuesType) => () => props.changeFilter(filter)
+    const addTaskOnKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && !isAddBtnDisabled && addTaskHandler()
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input
+                    placeholder="Please, enter title"
+                    value={title}
+                    onChange={setTitleHandler}
+                    //ref={taskTitleInput}
+                    onKeyDown={addTaskOnKeyPressHandler}
+                />
+                <button //onClick={addTaskHandler}
+                    disabled={isAddBtnDisabled}
+                    onClick={addTaskHandler}
+                 >+</button>
+                {titleMaxLengthWarning}
             </div>
             <ul>
                 {tasksJSXElements}
             </ul>
             <div>
-                <button onClick={() =>props.changeFilter("all")}>All</button>
-                <button onClick={() =>props.changeFilter("active")}>Active</button>
-                <button onClick={() =>props.changeFilter("completed")}>Completed</button>
+                <button onClick = {handlerCreator("all")}>All</button>
+                <button onClick = {handlerCreator("active")}>Active</button>
+                <button onClick = {handlerCreator("completed")}>Completed</button>
             </div>
         </div>
     )
