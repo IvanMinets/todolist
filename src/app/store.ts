0dec6688c6d50e6243f51caa0f7de7a39fac861f
IvanMinets <minetssmash@gmail.com>
2023-08-18 18:42:07 +0300

    updates

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
 import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
 import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer'
+import {authReducer} from 'features/Login/auth-reducer'
+import {configureStore} from "@reduxjs/toolkit";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -14,9 +15,10 @@ const rootReducer = combineReducers({
     auth: authReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const _store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export const store = configureStore({reducer: rootReducer})
+export type AppRootStateType = ReturnType<typeof store.getState>
 
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
 
