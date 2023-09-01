adfd4a9c8a9ed677ad5da56781af5fed59116f6a
IvanMinets <minetssmash@gmail.com>
2023-09-01 20:25:34 +0300

    Update handle-server-app-error.ts

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -1,12 +1,15 @@
-import { Dispatch } from "redux";
-import { appActions } from "app/app.reducer";
-import { BaseResponseType } from "common/types/common.types";
+import {Dispatch} from "redux";
+import {appActions} from "app/app.reducer";
+import {BaseResponseType} from "common/types/common.types";
 
-export const handleServerAppError = <D>(data: BaseResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }));
-  } else {
-    dispatch(appActions.setAppError({ error: "Some error occurred" }));
-  }
-  dispatch(appActions.setAppStatus({ status: "failed" }));
+export const handleServerAppError = <D>(data: BaseResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
+    if (showError) {
+        if (data.messages.length) {
+            dispatch(appActions.setAppError({error: data.messages[0]}));
+        } else {
+            dispatch(appActions.setAppError({error: "Some error occurred"}));
+        }
+        dispatch(appActions.setAppStatus({status: "failed"}));
+    }
+  dispatch(appActions.setAppStatus({status: "failed"}));
 };
